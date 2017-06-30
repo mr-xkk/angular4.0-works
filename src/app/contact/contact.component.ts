25442c38da6c634643e55e91759c4734b6b2dd2c
mr_xkk <xkxk.5038920>
2017-06-30 09:30:22 +0800

    form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -11,11 +11,29 @@ import { fadeIn } from '../animations/animate_show';
   ]
 })
 export class ContactComponent implements OnInit {
-  myTim:boolean = false;
-  myWe:boolean = false;
+  myTim : boolean = false;
+  myWe : boolean = false;
+  eTxt : string;
+  alertText : string;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  //提交函数
+  sub(){
+    console.log(this.eTxt);
+    let patten2=new RegExp(/^[0-9|A-z|_]{1,17}[@][0-9|A-z]{1,3}.(com)$/);   
+　　if(patten2.test(this.eTxt))     
+　　{          
+      this.alertText = "输入正确";
+　　}   
+　　else      
+　　{   
+      this.alertText = "请输入正确的邮箱地址"; 
+      return false;     
+　　}
+    document.forms[0].submit();
   }
 
 }
